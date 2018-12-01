90b1b7eaf9117a5541bfd8ff221547aa793f7e7e
Nick Erdmann <n@nirf.de>
2018-12-01 15:57:56 +0100

    use set instead of list

--- 01/01.hs
+++ 01/01.hs
@@ -1,17 +1,19 @@
+import Data.Set
+
 readFreqs f = do
     freqsStr <- fmap lines $ readFile f
     return (fmap parseInt freqsStr)
         where parseInt x = if (head x) == '+' then read (tail x) else read x
 
 findDupFreq cur freqs known =
-    if next `elem` known then
+    if next `member` known then
         next
     else
-        findDupFreq next (tail freqs) (next:known)
+        findDupFreq next (tail freqs) (insert next known)
     where
         next = cur + (head freqs)
 
 main = do
     freqs <- readFreqs "input"
     print $ sum freqs
-    print $ findDupFreq 0 (cycle freqs) [0]
+    print $ findDupFreq 0 (cycle freqs) (fromList [0])
--- 01/README.md
+++ 01/README.md
@@ -1,5 +1,5 @@
 # Day 1
-My first program in haskell. The second part takes over a minute to run on my computer.
+My first program in haskell.
 ```
 # copy puzzle input to ./input
 ghc 01.hs
