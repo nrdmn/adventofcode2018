0a2a520a1bec3eb42eecfde52b84590cf1cf9d82
Nick Erdmann <n@nirf.de>
2018-12-02 20:37:11 +0100

    cleanup

--- day02/day02.hs
+++ day02/day02.hs
@@ -2,14 +2,13 @@ import System.Environment
 import qualified Data.Map.Strict as Map
 
 countOccurences l = foldl (\acc x -> Map.insertWith (+) x 1 acc) Map.empty l
-containsTwice l = Map.filter (\x -> x == 2) (countOccurences l) /= Map.empty
-containsThrice l = Map.filter (\x -> x == 3) (countOccurences l) /= Map.empty
-diff a b = foldl (\acc x -> acc + (fromEnum $ fst x /= snd x)) 0 (zip a b)
+containsN n l = Map.filter (== n) (countOccurences l) /= Map.empty
+diff a b = foldl (\acc x -> acc + (fromEnum $ uncurry (/=) x)) 0 (zip a b)
 pairs [] = []
 pairs (x:xs) = map ((,) x) xs ++ pairs xs
 
 main = do
     args <- getArgs
     ids <- lines <$> readFile (head args)
-    print $ uncurry (*) $ foldl (\acc x -> (fst acc + fromEnum (containsTwice x), snd acc + fromEnum (containsThrice x))) (0, 0) ids
-    print $ map fst $ filter (\x -> fst x == snd x) $ uncurry zip $ head $ filter (\x -> diff (fst x) (snd x) == 1) (pairs ids)
+    print $ uncurry (*) $ foldl (\acc x -> (fst acc + fromEnum (containsN 2 x), snd acc + fromEnum (containsN 3 x))) (0, 0) ids
+    print $ fst $ unzip $ filter (uncurry (==)) $ uncurry zip $ head $ filter (\x -> 1 == uncurry diff x) (pairs ids)
