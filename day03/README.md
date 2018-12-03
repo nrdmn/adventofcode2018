b3e056986cbe44c4faf03ba36e5f42d4545b2839
Nick Erdmann <n@nirf.de>
2018-12-03 08:23:38 +0100

    add day03

---
+++ day03/README.md
@@ -0,0 +1,5 @@
+# Day 3
+```
+ghc day03.hs
+./day03 input
+```
---
+++ day03/day03.hs
@@ -0,0 +1,21 @@
+import System.Environment
+import qualified Data.Map.Strict as Map
+import Data.Char (isDigit)
+
+data Square = Square {sqid :: Int, left :: Int, top :: Int, width :: Int, height :: Int } deriving (Show)
+
+parseSquare x = Square (sq!!0) (sq!!1) (sq!!2) (sq!!3) (sq!!4)
+    where sq = read <$> (words $ map replace x)
+          replace x
+            | not (isDigit x) = ' '
+            | otherwise       = x
+
+coverage s = foldl (\acc x -> cover x acc) Map.empty s
+    where area x = [(a,b)|a<- [left x .. left x + width x - 1], b <- [top x .. top x + height x - 1]] 
+          cover sq a = foldl (\acc x -> Map.insertWith (+) x 1 acc) a (area sq)
+
+main = do
+    args <- getArgs
+    squares <- (fmap parseSquare) <$> lines <$> readFile (head args)
+    print $ length $ Map.filter (> 1) (coverage squares)
+    print $ sqid . fst <$> filter (\x -> Map.isSubmapOf (snd x) (coverage squares)) (map (\x -> (id x, coverage [x])) squares)
