e95cf41ca72e7ca73d3289e011c017f0b02584bb
Nick Erdmann <n@nirf.de>
2018-12-03 11:19:19 +0100

    faster hashing

--- day03/day03.hs
+++ day03/day03.hs
@@ -1,6 +1,8 @@
 import System.Environment
-import qualified Data.Map.Strict as Map
+import qualified Data.IntMap.Strict as Map
 import Data.Char (isDigit)
+
+hash (a,b) = 1000*a + b
 
 data Square = Square {sqid :: Int, left :: Int, top :: Int, width :: Int, height :: Int } deriving (Show)
 
@@ -12,7 +14,7 @@ parseSquare x = Square (sq!!0) (sq!!1) (sq!!2) (sq!!3) (sq!!4)
 
 coverage s = foldl (\acc x -> cover x acc) Map.empty s
     where area x = [(a,b)|a<- [left x .. left x + width x - 1], b <- [top x .. top x + height x - 1]] 
-          cover sq a = foldl (\acc x -> Map.insertWith (+) x 1 acc) a (area sq)
+          cover sq a = foldl (\acc x -> Map.insertWith (+) (hash x) 1 acc) a (area sq)
 
 main = do
     args <- getArgs
