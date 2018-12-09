b168997329ac268f6d0c29219a6e68c993e51541
Nick Erdmann <n@nirf.de>
2018-12-09 10:02:20 +0100

    add day09 part 2

--- day09/README.md
+++ day09/README.md
@@ -1,9 +1,13 @@
 # Day 9
-This ate about 5 GB of RAM.
+My first attempt with lists used about 5 GB of RAM for the first part. For part 2 I changed it to use sequences instead, which still requires about 3 GB.
+
+Because containers >= 0.5.8 wasn't installed on my system, I used cabal.
 ```
-ghc -O3 day09.hs
-./day09 input
+cabal update
+cabal install --only-dependencies
+cabal run input
 ```
 ```
 398048
+3180373421
 ```
--- day09/day09.hs
+++ day09/day09.hs
@@ -1,31 +1,30 @@
 import System.Environment
 import Data.List
+import qualified Data.Sequence as Seq
 
-insertAt pos i l = let (xs,ys) = splitAt pos l in xs ++ [i] ++ ys
-updateAt pos i l = let (xs,ys) = splitAt pos l in xs ++ [i] ++ (tail ys)
-removeAt pos l = let (xs,ys) = splitAt pos l in xs ++ (tail ys)
-addAt pos i l = updateAt pos (l!!pos + i) l
-
-data Game = Game { scores :: [Int], circle :: [Int], current :: Int } deriving (Show)
+data Game = Game { scores :: Seq.Seq Int, circle :: Seq.Seq Int, current :: Int } deriving (Show)
 
 play game marble
     | marble `mod` 23 == 0 =
         game {
-            scores  = addAt (marble `mod` (length $ scores game)) (circle game !! nextpos23 + marble) (scores game),
-            circle  = removeAt nextpos23 (circle game),
+            scores  = Seq.adjust (+ (circle game `Seq.index` nextpos23 + marble)) (marble `mod` (length $ scores game)) (scores game),
+            circle  = Seq.deleteAt nextpos23 (circle game),
             current = nextpos23
         }
     | otherwise =
         game {
-            circle  = insertAt nextpos marble (circle game),
+            circle  = Seq.insertAt nextpos marble (circle game),
             current = nextpos
         }
-        where nextpos = (current game + 1) `mod` (length (circle game)) + 1
-              nextpos23 = (current game - 7) `mod` (length (circle game))
+        where nextpos = (current game + 1) `mod` (Seq.length (circle game)) + 1
+              nextpos23 = (current game - 7) `mod` (Seq.length (circle game))
 
-part1 (players, marbles) = maximum $ scores $ foldl' play (Game (replicate players 0) [0] 0) [1..marbles]
+part1 (players, marbles) = maximum $ scores $ foldl' play (Game (Seq.replicate players 0) (Seq.singleton 0) 0) [1..marbles]
+
+part2 (players, marbles) = part1 (players, marbles*100)
 
 main = do
     args <- getArgs
     input <- (\x -> (x!!0, x!!6)) <$> fmap read <$> words <$> readFile (head args)
     print $ part1 input
+    print $ part2 input
